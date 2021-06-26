dae92ef1b26052fc614acfd6ff054023a13192ff
Asmir Abdimazhit <asmir8717@gmail.com>
2021-06-26 16:56:12 +0900

    ethers js initial commands

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+import {ethers} from "ethers";
+import abi from './abi.json';
+
+const MetaMaskInterface = () => {
+  let contractAddress = "0xA8fb9802fD8377FF120c6544b8DdE4a6f2EAc5EE";
+  let provider = null;
+
+  useEffect(() => {
+    if(typeof window.ethereum !== 'undefined') {
+      // Ethereum user detected. You can now use the provider.
+      provider = window['ethereum']
+      console.log('metamask found');
+    }
+    provider.enable()
+    .then(function (accounts) {
+      let ethersProvider = new ethers.providers.Web3Provider(provider);
+      let contract = new ethers.Contract(contractAddress, abi, ethersProvider.getSigner());
+      console.log(accounts);
+      let value = ethers.utils.bigNumberify(Math.pow(10,9)).mul(ethers.utils.bigNumberify(Math.pow(10,9))).mul(ethers.utils.bigNumberify(10));
+      let transaction = contract.transfer("0x8abaD0176217D8cF1a9fc9E559D30BfF36269737", value);
+      console.log(transaction);
+    })
+    .catch(function (error) {
+      // Handle error. Likely the user rejected the login
+      console.error(error)
+    })
+  });
+
+  return (
+    <p>hi</p>
+  )
+};
 
 function App() {
+  // const [contract, setContract] = useState("");
+  let contractAddress = "0x63DB8621A04471FcaBe07aE4c26AF8DD442F59e8";
+  let provider = null;
+
+  useEffect(() => {
+    if(typeof window.ethereum !== 'undefined') {
+      // Ethereum user detected. You can now use the provider.
+      provider = window['ethereum']
+      console.log('metamask found');
+    }
+    provider.enable()
+    .then(function (accounts) {
+      let ethersProvider = new ethers.providers.Web3Provider(provider);
+      console.log(accounts);
+      
+      let contract = new ethers.Contract(contractAddress, abi, ethersProvider.getSigner());
+      console.log(contract);
+      const {getSecret} = contract;
+      getSecret().then((secret) => console.log(secret));
+      // let value = ethers.utils.bigNumberify(Math.pow(10,9)).mul(ethers.utils.bigNumberify(Math.pow(10,9))).mul(ethers.utils.bigNumberify(10));
+      // let transaction = contract.transfer("0x8abaD0176217D8cF1a9fc9E559D30BfF36269737", value);
+      // console.log(transaction);
+    })
+    .catch(function (error) {
+      // Handle error. Likely the user rejected the login
+      console.error(error)
+    })
+
+    //getAccounts();
+    // MetaMaskInterface();
+
+  },[]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
