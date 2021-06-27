1de687564bfc88488658168451a086216e75b8c7
Asmir Abdimazhit <asmir8717@gmail.com>
2021-06-27 13:30:59 +0900

    Everything, but MyItems

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 
 
-let contractAddress = "0xb6c1c796A580525Af7Ad4474A34a63debE80221c";
+
 let nftItemAddress = "0x3C29ee95B72eAd2E6246d100948Ab2780d1c9671";
 let nftMarketplaceAddress = "0xc40cbE61A51cbD0A39FEe26c3EEb301A28C3bdAB";
 
@@ -55,6 +55,7 @@ function ProductPutComponent() {
 
   return (
     <Card className = "BuyComponent">
+      <Card.Title>Place you item for SALE!</Card.Title>
       <input
         className = "BuyComponentInput" 
         onChange = {e => setUriValue(e.target.value)}
@@ -167,32 +168,7 @@ function MarketItemCard(props) {
 
 
 function App() {
-  const [uriValue, setUriValue] = useState("");
   const [account, setAccount] = useState();
-
-  async function requestAccount() {
-    await window.ethereum.request({method : "eth_requestAccounts"});
-  }
-
-  async function getMarketBalance() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const balance = await provider.getBalance("0x8212973BcC0426034ea950E1186Bf50d98fD9f2f");
-    console.log("The market balance is: ", balance);
-  }
-
-
-  async function getMarketItems() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const nftMarketContract = new ethers.Contract(nftMarketplaceAddress, nftMarketplaceABI, signer);
-    try {
-      const marketItems = await nftMarketContract.fetchMarketItems();
-      console.log(marketItems);
-      return marketItems;
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   async function getAccountDetails() {
     if (typeof window.ethereum !== 'undefined') {
@@ -205,71 +181,11 @@ function App() {
     }
   }
 
-  async function fetchURI() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const nftItemContract = new ethers.Contract(nftItemAddress, nftItemABI, provider);
-      
-      try {
-        const fetchedURI = await nftItemContract.tokenURI(10);
-        const balance = await provider.listAccounts();
-        console.log("The uri is", JSON.stringify(fetchedURI));
-        console.log("The balance is:", JSON.stringify(balance[0]));
-      } catch(err) {
-        console.log("Error:", err);
-      }
-    }
-  }
-
-  async function listItem() {
-    //if (!uriValue) return;
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contractMarket = new ethers.Contract(nftMarketplaceAddress, nftMarketplaceABI, signer)
-      const transactionPutProduct = await contractMarket.createMarketItem(nftItemAddress, 23, "5000000000000000000" , {value : "10000000000000000"});
-      await transactionPutProduct.wait();
-    }
-  }
-
-  async function buyItem() {
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contractMarket = new ethers.Contract(nftMarketplaceAddress, nftMarketplaceABI, signer)
-      const transactionBuyProduct = await contractMarket.createMarketSale(nftItemAddress, 6, {value : "5000000000000000000"});
-      await transactionBuyProduct.wait();
-    }
-  }
-
-  async function setURI() {
-    if (!uriValue) return;
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(nftItemAddress, nftItemABI, signer);
-      const transaction = await contract.createToken(uriValue);
-      setUriValue("");
-      let transactionResult = await transaction.wait();
-      console.log("transaction result:", parseInt(transactionResult.events[0].args[2]._hex, 16));
-      fetchURI();//not one
-    }
-  }
-
-  
-
-
   useEffect(() => {
     getAccountDetails().then((accountDetails) => {
-      //console.log(accountDetails);
       setAccount(accountDetails);
     });
   }, []);
-
-
 
 
   return (
@@ -280,54 +196,11 @@ function App() {
         <h3>Your Account Balance: </h3>
         {account && <div>{account.balance}</div>}
       </header>
-      
-      {/* <MarketItemCard /> */}
-
-      {/* <input 
-        onChange = {e => setUriValue(e.target.value)}
-        placeholder = "Set URI"
-        value = {uriValue}
-      /> */}
-
       <ProductPutComponent />
-
       <MarketGallery />
-
-
     </div>
   );
 }
 
 export default App;
 
-
-
-const MetaMaskInterface = () => {
-  let contractAddress = "0xA8fb9802fD8377FF120c6544b8DdE4a6f2EAc5EE";
-  let provider = null;
-
-  useEffect(() => {
-    if(typeof window.ethereum !== 'undefined') {
-      // Ethereum user detected. You can now use the provider.
-      provider = window['ethereum']
-      console.log('metamask found');
-    }
-    provider.enable()
-    .then(function (accounts) {
-      let ethersProvider = new ethers.providers.Web3Provider(provider);
-      let contract = new ethers.Contract(contractAddress, abi, ethersProvider.getSigner());
-      console.log(accounts);
-      let value = ethers.utils.bigNumberify(Math.pow(10,9)).mul(ethers.utils.bigNumberify(Math.pow(10,9))).mul(ethers.utils.bigNumberify(10));
-      let transaction = contract.transfer("0x8abaD0176217D8cF1a9fc9E559D30BfF36269737", value);
-      console.log(transaction);
-    })
-    .catch(function (error) {
-      // Handle error. Likely the user rejected the login
-      console.error(error)
-    })
-  });
-
-  return (
-    <p>hi</p>
-  )
-};
